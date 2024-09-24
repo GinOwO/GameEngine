b8a7a1592783ed5a658baab3d54e48f5e17bf072
GinOwO <65128789+GinOwO@users.noreply.github.com>
2024-09-24 07:59:20 +0530

    QoL

--- src/core/Engine.cpp
+++ src/core/Engine.cpp
@@ -87,7 +87,7 @@ void Engine::run()
 	int frames = 0;
 	double frame_counter = 0;
 	double frame_time = 1.0 / this->FRAME_CAP;
-	// glfwSwapInterval(frame_time);
+	// glfwSwapInterval(0); // Disable Vsync
 
 	timer.reset();
 	while (this->running) {
